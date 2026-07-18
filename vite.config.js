43ce67b9351934232dbae2a57e41f91e620d978f
xavics320 <xavierparedes296@gmail.com>
2026-07-18 11:25:06 +0200

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-    build: {
-    rollupOptions: {
-    output: {
-    manualChunks: {
-    'three': ['three'],
-    'vendor': ['react', 'react-dom', 'react-router-dom'],
-     }
-    }
-   }
-  }
+plugins: [react()],
 })
